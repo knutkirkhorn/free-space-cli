41b91f944e21208d475d4d0b603d104530b1541e
Knut Kirkhorn <knutkirk@hotmail.com>
2018-02-03 22:59:12 +0100

    Add module files

---
+++ cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict';
+const freeSpace = require('@knutkirkhorn/free-space');
+const meow = require('meow');
+const prettyBytes = require('pretty-bytes');
+
+const cli = meow(`
+        Usage
+          $ free-space <drive>
+        Examples
+          $ free-space
+          $ free-space C:
+          $ free-space \dev\sda
+`);
+
+const disk = cli.input[0];
+
+if (cli.input.length <= 1) {
+    freeSpace(disk).then(bytes => {
+        console.log('Free space: ' + prettyBytes(bytes));
+    });
+}
